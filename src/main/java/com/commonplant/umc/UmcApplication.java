0bcdecc1e89f33fb420166a25040ca7fb2d8955d
sonshn <sonshn202064@gmail.com>
2023-01-04 21:15:33 +0900

    [FEAT]: Add HelloController

--- src/main/java/com/commonplant/umc/UmcApplication.java
+++ src/main/java/com/commonplant/umc/UmcApplication.java
@@ -2,8 +2,9 @@ package com.commonplant.umc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class UmcApplication {
 
 	public static void main(String[] args) {
