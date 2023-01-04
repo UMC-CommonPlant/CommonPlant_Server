4e2b15c4f991e29d3a08b5869951e6408b171166
sonshn <sonshn202064@gmail.com>
2023-01-04 20:52:31 +0900

    Initial Commit

---
+++ src/test/java/com/commonplant/umc/UmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commonplant.umc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
