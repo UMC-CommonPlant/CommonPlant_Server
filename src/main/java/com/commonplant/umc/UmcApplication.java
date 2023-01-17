2c5f9257c148b0d1cc787dea2270bf3777bfd408
sonshn <sonshn202064@gmail.com>
2023-01-17 21:04:33 +0900

    [FEAT]: Add Column wateredDate in Table Plant

--- src/main/java/com/commonplant/umc/UmcApplication.java
+++ src/main/java/com/commonplant/umc/UmcApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class UmcApplication {
 
 	public static void main(String[] args) {
