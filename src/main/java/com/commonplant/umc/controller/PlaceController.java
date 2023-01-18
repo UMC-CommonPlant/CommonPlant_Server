ee5ebaca584bea06ba64d5467a6e496efaf423ed
dhchoi98 <chleogks9808@naver.com>
2023-01-19 07:52:01 +0900

    [FEAT] : Add Firebase Image upload API
    [FIX] : Update PlaceController, PlaceService

--- src/main/java/com/commonplant/umc/controller/PlaceController.java
+++ src/main/java/com/commonplant/umc/controller/PlaceController.java
@@ -2,6 +2,7 @@ package com.commonplant.umc.controller;
 
 
 import com.commonplant.umc.domain.Place;
+import com.commonplant.umc.domain.User;
 import com.commonplant.umc.dto.JsonResponse;
 import com.commonplant.umc.dto.place.PlaceRequest;
 import com.commonplant.umc.service.PlaceService;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
@@ -22,11 +24,11 @@ public class PlaceController {
     private final UserService userService;
 
     @PostMapping("/place/add")
-    public ResponseEntity<JsonResponse> addPlace(@RequestBody PlaceRequest.addPlace req){
+    public ResponseEntity<JsonResponse> addPlace(@RequestPart("place") PlaceRequest.addPlace req, @RequestPart("image") MultipartFile file){
 
         User user = userService.getUser(1l);
 
-        String placeCode = placeService.addPlace(user, req);
+        String placeCode = placeService.addPlace(user, req, file);
 
         return ResponseEntity.ok(new JsonResponse(true, 200,"addPlace", placeCode));
     }
--- src/main/java/com/commonplant/umc/service/PlaceService.java
+++ src/main/java/com/commonplant/umc/service/PlaceService.java
@@ -1,9 +1,11 @@
 package com.commonplant.umc.service;
 
 
+import com.commonplant.umc.config.FirebaseConfig;
 import com.commonplant.umc.config.exception.BadRequestException;
 import com.commonplant.umc.domain.Belong;
 import com.commonplant.umc.domain.Place;
+import com.commonplant.umc.domain.User;
 import com.commonplant.umc.dto.place.PlaceRequest;
 import com.commonplant.umc.repository.BelongRepository;
 import com.commonplant.umc.repository.PlaceRepository;
@@ -11,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import static com.commonplant.umc.config.exception.ErrorResponseStatus.PLACE_CODE_ERROR;
 
@@ -22,14 +25,18 @@ public class PlaceService {
 
     private final BelongRepository belongRepository;
 
-
+    private final FirebaseService firebaseService;
 
     @Transactional    // 유저랑 이미지쪽 완성되면 그 때 인자 추가
-    public String addPlace(User user, PlaceRequest.addPlace req)
+    public String addPlace(User user, PlaceRequest.addPlace req, MultipartFile file)
     {
         String newCode = randomCode();
 
-        String imgUrl = "img url test";
+        String imgUrl = null;
+
+        if(file.getSize()>0){
+            imgUrl = firebaseService.uploadFiles("commonPlant_"+newCode,file);
+        }
 
         Place place = Place.builder().name(req.getName()).address(req.getAddress())
                 .placeImgUrl(imgUrl).code(newCode).build();
