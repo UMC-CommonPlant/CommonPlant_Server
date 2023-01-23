b80ab30cfed5107153128147db38f4abefc3c78f
dhchoi98 <chleogks9808@naver.com>
2023-01-23 16:43:41 +0900

    feat : Add User domain, Repository, service

--- src/main/java/com/commonplant/umc/repository/UserRepository.java
+++ src/main/java/com/commonplant/umc/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.commonplant.umc.repository;
 
+import com.commonplant.umc.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    List<User> findByNameContains(String name);
 }
