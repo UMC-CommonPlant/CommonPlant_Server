7241d00cefa9780e3d71743415252bbc9dbaf373
sonshn <sonshn202064@gmail.com>
2023-01-23 14:32:38 +0900

    [FEAT]: Add getMemoCard()

--- src/main/java/com/commonplant/umc/controller/MemoController.java
+++ src/main/java/com/commonplant/umc/controller/MemoController.java
@@ -2,11 +2,15 @@ package com.commonplant.umc.controller;
 
 import com.commonplant.umc.dto.JsonResponse;
 import com.commonplant.umc.dto.memo.MemoRequest;
+import com.commonplant.umc.dto.memo.MemoResponse;
+import com.commonplant.umc.dto.plant.PlantResponse;
 import com.commonplant.umc.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -26,12 +30,28 @@ public class MemoController {
         return ResponseEntity.ok(new JsonResponse(true, 200, "addMemo", memoTest));
     }
 
-    // 메모 조회 (GET)
+    // 특정 메모 조회 (GET)
+    @GetMapping("/memo/card/{memoIdx}")
+    public ResponseEntity<JsonResponse> getMemoCard(@PathVariable Long memoIdx) {
+
+        System.out.println("=============GET MEMO TEST.NAME===============");
+
+        MemoResponse.memoCardRes res = memoService.getMemoCard(memoIdx);
+
+        return ResponseEntity.ok(new JsonResponse(true,200, "getMemoCard", res));
+    }
+
+    // 메모 리스트 조회 (GET)
     // 식물 페이지/캘린더 뷰: 식물 페이지 기준으로 구성!
     // 식물 별로 조회해야 하는데... plant 테이블의 plant_idx 사용?
 
-    // @GetMapping("/plant/")
-
+//    @GetMapping("/plant/{plantIdx}/memoList")
+//    public ResponseEntity<JsonResponse> getMemoList(@PathVariable Long plantIdx){
+//
+//        System.out.println("=============GET MEMO LIST TEST.NAME===============" + req.getUser() + req.getContent());
+//
+//        return ResponseEntity.ok(new JsonResponse(true, 200, "getMemoList", res));
+//    }
 
     // 메모 수정 (PATCH: plant랑 user는 업데이트 되지 않음)
     @PatchMapping("/memo/update/{memoIdx}")
